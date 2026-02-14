a8689ac1ea7fc3fc0df62f9371a61de609abcfbe
solanabettercall <solanabettercall@gmail.com>
2026-02-14 14:31:37 +0000

    wscat

--- home.nix
+++ home.nix
@@ -5,6 +5,7 @@
     ./modules/programs/zsh.nix
     ./modules/programs/git.nix
     ./modules/programs/cli-tools.nix
+    ./modules/programs/network.nix
   ];
 
   home.stateVersion = "25.05";
--- modules/programs/network.nix
+++ modules/programs/network.nix
@@ -4,5 +4,10 @@
   home.packages = with pkgs; [
     httpie
     nmap
+    nodejs_24
   ];
+
+  home.shellAliases = {
+    wscat = "npx wscat";
+  };
 }
